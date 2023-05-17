20a08409d3112bfdaf2b56509b85458e84469c26
danilonicacio <danilonicacio2016@gmail.com>
2023-05-17 11:04:11 -0400

    modified:   src/app/app-routing.module.ts 	modified:   src/app/app.component.html 	modified:   src/app/app.module.ts 	new file:   src/app/courses/courses-routing.module.ts 	new file:   src/app/courses/courses.module.ts 	new file:   src/app/courses/courses/courses.component.html 	new file:   src/app/courses/courses/courses.component.scss 	new file:   src/app/courses/courses/courses.component.spec.ts 	new file:   src/app/courses/courses/courses.component.ts 	new file:   src/app/courses/model/course.ts 	new file:   src/app/courses/services/courses.service.spec.ts 	new file:   src/app/courses/services/courses.service.ts 	new file:   src/app/shared/app-material/app-material.module.ts   new file:   src/app/shared/components/error-dialog/error-dialog.component.html 	new file:   src/app/shared/components/error-dialog/error-dialog.component.scss 	new file:   src/app/shared/components/error-dialog/error-dialog.component.spec.ts 	new file:   src/app/shared/components/error-dialog/error-dialog.component.ts 	new file:   src/app/shared/shared.module.ts 	new file:   src/assets/courses.json

---
+++ src/app/shared/app-material/app-material.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import {MatButtonModule} from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+
+@NgModule({
+  exports: [
+    MatCardModule,
+    MatTableModule,
+    MatToolbarModule,
+    MatProgressSpinnerModule,
+    MatButtonModule,
+    MatDialogModule
+  ]
+})
+export class AppMaterialModule { }
