5aaedb8cfd7469d0c5166d159b1c6024dc8b6c87
danilonicacio <danilonicacio2016@gmail.com>
2023-05-18 08:54:39 -0400

    modified:   src/app/courses/courses/courses.component.html 	modified:   src/app/courses/services/courses.service.ts 	modified:   src/app/shared/app-material/app-material.module.ts 	new file:   src/app/shared/pipes/category.pipe.spec.ts 	new file:   src/app/shared/pipes/category.pipe.ts 	modified:   src/app/shared/shared.module.ts 	modified:   src/assets/courses.json

--- src/app/shared/app-material/app-material.module.ts
+++ src/app/shared/app-material/app-material.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import {MatButtonModule} from '@angular/material/button';
-import { MatDialogModule } from '@angular/material/dialog';
 
 @NgModule({
   exports: [
@@ -13,7 +14,8 @@ import { MatDialogModule } from '@angular/material/dialog';
     MatToolbarModule,
     MatProgressSpinnerModule,
     MatButtonModule,
-    MatDialogModule
+    MatDialogModule,
+    MatIconModule
   ]
 })
 export class AppMaterialModule { }
